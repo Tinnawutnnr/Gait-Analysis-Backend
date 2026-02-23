7f6a15659d4e4ef3bc07421f0cb6f2209405e06c
Tinnawutnnr <tinnawut.noin@kmutt.ac.th>
2026-02-23 14:54:55 +0700

    fix: remove duplicate create caretaker api

--- src/main/java/com/gaitanalyze/gaitanalysisapp/auth/RefreshTokenRepo.java
+++ src/main/java/com/gaitanalyze/gaitanalysisapp/auth/RefreshTokenRepo.java
@@ -1,9 +1,7 @@
 package com.gaitanalyze.gaitanalysisapp.auth;
 
 import com.gaitanalyze.gaitanalysisapp.caretaker.Caretaker;
-import org.antlr.v4.runtime.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
